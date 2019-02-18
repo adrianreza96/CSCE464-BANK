8b22b8f3fa366cdf9b18faf5263f07142d7ba437
Adam Schlichtmann <adam.schlichtmann1113@gmail.com>
2019-02-17 22:41:56 -0600

    Added the review pages and the skeleton of the required servlet

---
+++ WebContent/CustomerReview.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+	<body>
+		<link rel="stylesheet" type="text/css" href="NavigationBarTheme.css">
+		<div class="topnav">
+		  <a class="active" href="CustomerHomePage.jsp">Home</a>
+		  <a href="ViewOrders.jsp">Orders</a>
+		  <a href="Login.jsp" style="float:right">Log Out</a>
+		</div>
+		<div align="center">
+			<form action=CustomerReview>
+				<textarea col="150" rows="8" type=text placeholder="Enter your Review" name="review"></textarea><br>
+				Star Rating: <br>
+				<select>
+					<option value="0">0</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select>
+				<br>
+				<br>
+				<input type=submit value="Submit">
+			</form>
+			<form action="ConcertDetailsSelection.jsp">	
+				<input type=submit value="Cancel">
+			</form>
+		</div>
+	</body>
+</html>
---
+++ src/servlet/CustomerReview.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CustomerReview
+ */
+@WebServlet("/CustomerReview")
+public class CustomerReview extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomerReview() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Take information from the CustomerReview jsp page and add it to the ReviewDB
+		// redirect to page below but send show if sucessful or not based on database attempt
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("CustomerReviewConfirmation.jsp");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
